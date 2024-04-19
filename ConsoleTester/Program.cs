28a8a4000edf968bb681c48b52e606ef8f9a1d43
Verner Fortelius <verner@fortelius.net>
2024-04-19 22:46:25 +0300

    start adding stuff for testing allocations

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using vforteli.WordPieceTokenizer;
+
+Console.WriteLine("Tokenizing text...");
+
+var text = await File.ReadAllTextAsync("test_files/world192.txt");
+var tokenizer = new WordPieceTokenizer(VocabStrings.Vocab);
+
+
+var count = 100;
+
+for (int i = 0; i < count; i++)
+{
+    var foo = tokenizer.Tokenize(text);
+    Console.WriteLine(i);
+}
+
+
+Console.WriteLine("Done...");
---
+++ ConsoleTester/README.md
@@ -0,0 +1,7 @@
+# ConsoleTester
+
+Mainly used for allocation testing
+
+```
+dotnet counters monitor -- dotnet ConsoleTester.dll
+```
