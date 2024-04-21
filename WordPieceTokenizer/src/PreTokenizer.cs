5b75683cf24e79ccb746bec9c9e5a1963a11c65d
Verner Fortelius <verner@fortelius.net>
2024-04-21 11:56:24 +0300

    precompiled regex

--- WordPieceTokenizer/src/PreTokenizer.cs
+++ WordPieceTokenizer/src/PreTokenizer.cs
@@ -2,7 +2,7 @@
 
 namespace vforteli.WordPieceTokenizer;
 
-public class PreTokenizer
+public partial class PreTokenizer
 {
     /// <summary>
     /// Split text into words according to.. and.. um.. bertish
@@ -10,7 +10,10 @@ public class PreTokenizer
     public IEnumerable<Word> Split(ReadOnlySpan<char> text)
     {
         // todo maybe switch to something that just reads characters from a span instead...
-        var matches = Regex.Matches(text.ToString(), @"(\b\w+\b)|\p{P}");
+        var matches = WordBoundaryPattern().Matches(text.ToString());
         return matches.Select(o => new Word(o.Value, o.Index, o.Index + o.Length));
     }
+
+    [GeneratedRegex(@"(\b\w+\b)|\p{P}")]
+    private static partial Regex WordBoundaryPattern();
 }
